651b83e1da18400dc7f2347c92be1e28d031cfda
ObukhovaKatya <bird.of.cry@gmail.com>
2025-08-15 12:34:43 +0300

    Add files via upload

--- test_create_order.py
+++ test_create_order.py
@@ -1,21 +1,15 @@
+# Екатерина Обухова, 33 когорта - Финальный проект
 import sender_stand_request
 import data
 
-def test_create_order_and_get_by_track():
-    # 1. Создаем заказ
-    create_response = sender_stand_request.post_new_order(data.create_order_body)
-    assert create_response.status_code == 201, "Заказ не создан"
-    
-    # 2. Получаем трек заказа
-    track_number = create_response.json().get("track")
-    assert track_number is not None, "Track не получен"
+def test_track_order():
+    # Создаём заказ и получаем трек
+    create_resp = sender_stand_request.post_new_order(data.create_order_body)
+    track_number = create_resp.json().get("track")
 
-    # 3. Получаем заказ по треку
-    get_response = sender_stand_request.get_track_order(track_number)
-    assert get_response.status_code == 200, "Заказ по треку не найден"
+    # Получаем заказ по треку
+    get_resp = sender_stand_request.get_track_order(track_number)
 
-    # 4. Проверяем данные заказа
-    order_info = get_response.json().get("order")
-    assert order_info["firstName"] == data.create_order_body["firstName"]
-    assert order_info["lastName"] == data.create_order_body["lastName"]
-    assert order_info["address"] == data.create_order_body["address"]
+    # Заказ найден по треку
+    assert get_resp.status_code == 200
+
