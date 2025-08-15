3b9072dab5092d0b5759f54275ffd9aebe1a8934
ObukhovaKatya <bird.of.cry@gmail.com>
2025-08-15 12:32:55 +0300

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://4b505d05-1b5f-4b0d-9873-e3fe9839db4a.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://481db4bf-8f80-4eec-8c91-393e233567bd.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders/"
 TRACK_ORDER = "/api/v1/orders/track?t="
